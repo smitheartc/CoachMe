2769f5deaba5783c3c7a11cdca2e8af8b08ef644
Caleb Smitheart <smitheartc@gmail.com>
2025-02-24 13:19:58 -0600

    Centered a div lol

--- CoachMe_Frontend/src/Navbar.jsx
+++ CoachMe_Frontend/src/Navbar.jsx
@@ -3,8 +3,8 @@ import {Link} from 'react-router-dom';
 
 export default function Navbar() {
     return (
-      <header className="flex justify-between items-center p-4 bg-white shadow-md">
-        <div className="flex items-center space-x-4">
+      <header className="flex justify-center items-center p-4 bg-white shadow-md">
+        <div className="flex justify-between items-center space-x-4">
           <img src={logo} className="h-12 md:h-20 w-auto" alt="Logo" />
           <Link to="/"className="px-4 py-2 bg-gray-200 rounded hover:bg-gray-300">Home</Link>
           <Link to="/coachfinder" className="px-4 py-2 bg-gray-200 rounded hover:bg-gray-300">Find a Coach</Link>
