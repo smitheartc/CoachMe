00424b61c9acc8277c858ff065752cfd288af3d7
pravan <pravangorthy21@gmail.com>
2025-04-30 21:04:14 -0500

    new frontend developments added

--- CoachMe_Backend/api/createBooking.py
+++ CoachMe_Backend/api/createBooking.py
@@ -1,24 +1,24 @@
 from flask_restful import Api, Resource, reqparse
-from prisma.models import Booking
+# # from prisma.models import Booking
 
-class bookingApi(Resource):
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('clientId', type=int)
-        parser.add_argument('coachId', type=int)
-        parser.add_argument('datetime', type=To_Be_Determined) 
+# class bookingApi(Resource):
+#     def post(self):
+#         parser = reqparse.RequestParser()
+#         parser.add_argument('clientId', type=int)
+#         parser.add_argument('coachId', type=int)
+#         parser.add_argument('datetime', type=To_Be_Determined) 
 
-        args = parser.parse_args()
+#         args = parser.parse_args()
 
-        clientId = args['clientId']
-        coachId = args['coachId']
-        time = args['datetime']        
+#         clientId = args['clientId']
+#         coachId = args['coachId']
+#         time = args['datetime']        
 
-        Booking.prisma().create(
-            data={
-                'clientId' : clientId,
-                'coachId' : coachId,
-                'bookingTime' : time
-            }
-        )
+#         Booking.prisma().create(
+#             data={
+#                 'clientId' : clientId,
+#                 'coachId' : coachId,
+#                 'bookingTime' : time
+#             }
+#         )
         
