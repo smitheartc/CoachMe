6de6b33d5cceec95311b923a2ac146525337cd12
jordanjoelson <jjoelson007@gmail.com>
2025-07-15 14:13:26 -0500

    backend frontend connections

--- CoachMe_Frontend/vite.config.js
+++ CoachMe_Frontend/vite.config.js
@@ -5,4 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    proxy: {
+      '/login': 'http://localhost:3000',
+      '/callback': 'http://localhost:3000',
+      '/logout': 'http://localhost:3000',
+      // Add other backend routes as needed
+    }
+  }
 })
